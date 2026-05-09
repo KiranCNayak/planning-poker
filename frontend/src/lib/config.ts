01a49b6b21c6a0f42e08e8151d53ab9bc3ecab25
Kiran C Nayak <33034799+KiranCNayak@users.noreply.github.com>
2026-05-09 22:30:51 +0530

    Format

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -2,5 +2,7 @@ const browserOrigin = `${window.location.protocol}//${window.location.hostname}`
 
 export const API_BASE_URL =
 	import.meta.env.VITE_API_BASE_URL ?? `${browserOrigin}:4000`;
-export const SOCKET_BASE_URL = import.meta.env.VITE_SOCKET_BASE_URL ?? API_BASE_URL;
-export const SHARE_BASE_URL = import.meta.env.VITE_SHARE_BASE_URL ?? window.location.origin;
+export const SOCKET_BASE_URL =
+	import.meta.env.VITE_SOCKET_BASE_URL ?? API_BASE_URL;
+export const SHARE_BASE_URL =
+	import.meta.env.VITE_SHARE_BASE_URL ?? window.location.origin;
